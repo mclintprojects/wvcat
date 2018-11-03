526acb01cb31e610fd3891037af1a3754fe1133d
Mbah Clinton <clintonmbah44@gmail.com>
2018-11-03 17:54:08 +0000

    added speech recognition

--- wvcat.js
+++ wvcat.js
@@ -3,14 +3,34 @@
 
 	this.controls = [];
 
-	this.initialize = function() {
+	this.initialize = function(options) {
+		this.options = options || { lang: 'en-US' };
+		if ('webkitSpeechRecognition' in window) {
+			attachRecognitionContainerToDocument();
+			startSpeechRecognizer();
+			findControllableElementsInDocument();
+		} else console.error('This browser does not support voice control.');
+	};
+
+	function attachRecognitionContainerToDocument() {
+		const card = document.createElement('div');
+		card.classList.add('wvcat-recognition-container');
+
+		this.recognitionText = document.createElement('p');
+		recognitionText.appendChild(document.createTextNode('Sample recognition'));
+		card.appendChild(recognitionText);
+
+		document.body.appendChild(card);
+	}
+
+	function findControllableElementsInDocument() {
 		const elements = document.getElementsByClassName('wvcat-element');
 		for (let i = 0; i < elements.length; i++) {
 			let e = elements[i];
 
 			if (e.dataset.wvcatId) {
 				controls.push({
-					identifier: e.dataset.wvcatId.replace('-', ' '),
+					identifier: e.dataset.wvcatId,
 					name: e.localName,
 					type: e.type
 				});
@@ -18,5 +38,30 @@
 		}
 
 		console.log(controls);
-	};
+	}
+
+	function startSpeechRecognizer() {
+		setText('Listening for commands...');
+		this.recognizer = new webkitSpeechRecognition();
+		recognizer.lang = this.options.lang;
+		recognizer.start();
+		recognizer.onresult = generateTranscript;
+	}
+
+	function setText(text) {
+		recognitionText.innerText = text;
+	}
+
+	function generateTranscript({ results }) {
+		setText('Parsing command..');
+		let transcript = '';
+		for (let i = 0; i < results.length; i++) {
+			const result = results[i];
+			if (result.isFinal) transcript += result[0].transcript;
+		}
+
+		setText(`Executing command -> [${transcript}]`);
+		transcript = '';
+		window.setTimeout(startSpeechRecognizer, 2000);
+	}
 })(window);
