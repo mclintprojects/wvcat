5574ccd315f25708add4d6f90e1965f0f40c636c
Mbah Clinton <clintonmbah44@gmail.com>
2019-02-07 10:33:44 +0000

    fixed add punctuations matcher with regex

--- js/wvcat.js
+++ js/wvcat.js
@@ -10,7 +10,7 @@
 	this.currentControlIndex = 0;
 
 	this.initialize = function(options) {
-		this.options = options || { lang: 'en-ZA' };
+		this.options = options || { lang: 'en-GH' };
 
 		findControllableElementsInDocument();
 
@@ -150,21 +150,22 @@
 
 	function addPunctuations(transcript) {
 		return transcript
-			.replace(' at sign ', '@')
-			.replace(' and sign ', '&')
-			.replace(' question mark ', '?')
-			.replace(' comma ', ',')
-			.replace(' new line ', '\r\n')
-			.replace(' equal sign ', '=')
-			.replace(' hyphen ', '-')
-			.replace(' underscore ', '_')
-			.replace(' plus sign ', '+')
-			.replace(' forward slash ', '/')
-			.replace(' single quote ', "'")
-			.replace(' percent sign ', '%')
-			.replace(' left paranthesis ', '(')
-			.replace(' right paranthesis ', ')')
-			.replace(' quote sign ', '"');
+			.replace(/^\s(at sign)\s*/g, '@')
+			.replace(/^\s(and sign)\s*/g, '&')
+			.replace(/^\s(question mark)\s*/g, '?')
+			.replace(/^\s(comma)\s*/g, ',')
+			.replace(/^\s(new line)\s*/g, '\r\n')
+			.replace(/^\s(equal sign)\s*/g, '=')
+			.replace(/^\s(hyphen)\s*/g, '-')
+			.replace(/^\s(underscore)\s*/g, '_')
+			.replace(/^\s(plus sign)\s*/g, '+')
+			.replace(/^\s(forward slash)\s*/g, '/')
+			.replace(/^\s(back slash)\s*/g, '\\')
+			.replace(/^\s(single quote)\s*/g, "'")
+			.replace(/^\s(percent sign)\s*/g, '%')
+			.replace(/^\s(left parenthesis)\s*/g, '(')
+			.replace(/^\s(right parenthesis)\s*/g, ')')
+			.replace(/^\s(quote sign)\s*/g, '"');
 	}
 
 	function executeCommand(transcript) {
